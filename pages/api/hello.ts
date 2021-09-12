c09b5bb070256be4dd5b17c1399cc84dc0bf1a6d
htbkoo <h1017760@connect.hku.hk>
2021-09-13 00:48:31 +0800

    added reference implementation from  `graphql-js` (reference: https://graphql.org/graphql-js/)

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { buildSchema, graphql } from "graphql";
+
 type Data = {
   name: string
 }
@@ -8,3 +10,23 @@ type Data = {
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
